eaa311db1e099c6164384c0616144edb483da22e
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2016-10-21 10:36:06 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -13,7 +13,7 @@ echo "brew start"
 brew update
 
 # Upgrade any already-installed formulae.
-brew upgrade --all
+brew upgrade
 
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
