b850411bc0ea20feff720fa56778a0e3aa4abfbf
Anders Olsen Sandvik <github@mlsv.no>
2024-05-07 23:31:03 +0200

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,16 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
+git pull origin main;
 
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE" -avh --no-perms . ~;
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
 
